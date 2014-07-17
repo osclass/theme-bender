cd5978b7a64c153a9f4e9b29f133a54bab53a209
Carlos <garciademarina@gmail.com>
2014-07-17 14:16:13 +0200

    bug fixed, bender_draw_categories_list function

--- functions.php
+++ functions.php
@@ -222,10 +222,15 @@ FUNCTIONS
         <ul class="r-list">
              <li>
                  <h1>
-                    <?php if ( osc_count_subcategories() > 0 ) { ?>
+                    <?php
+                    $_slug      = osc_category_slug();
+                    $_url       = osc_search_category_url();
+                    $_name      = osc_category_name();
+                    $_total_items = osc_category_total_items();
+                    if ( osc_count_subcategories() > 0 ) { ?>
                     <span class="collapse resp-toogle"><i class="fa fa-caret-right fa-lg"></i></span>
                     <?php } ?>
-                    <a class="category <?php echo osc_category_slug() ; ?>" href="<?php echo osc_search_category_url() ; ?>"><?php echo osc_category_name() ; ?></a> <span>(<?php echo osc_category_total_items() ; ?>)</span>
+                    <a class="category <?php echo $_slug; ?>" href="<?php echo $_url; ?>"><?php echo $_name ; ?></a> <span>(<?php echo $_total_items ; ?>)</span>
                  </h1>
                  <?php if ( osc_count_subcategories() > 0 ) { ?>
                    <ul>
